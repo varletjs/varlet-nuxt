f15a197d8e7da2ff7c1ff2bccb073c4bfa1829cd
lq-c <2404831604@qq.com>
2024-03-11 00:13:55 +0800

    feat: add demo

---
+++ playground/pages/component/breadcrumbs.vue
@@ -0,0 +1,7 @@
+<template>
+  <var-breadcrumbs separator="\">
+    <var-breadcrumb>首页</var-breadcrumb>
+    <var-breadcrumb>二级</var-breadcrumb>
+    <var-breadcrumb>三级</var-breadcrumb>
+  </var-breadcrumbs>
+</template>
---
+++ playground/pages/component/icon.vue
@@ -0,0 +1,3 @@
+<template>
+  <var-icon name="https://varlet.gitee.io/varlet-ui/cat.jpg" :size="32" />
+</template>
