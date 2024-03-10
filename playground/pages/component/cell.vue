252a5c79e61c64e0ee4b5c2330d4d0291bd1b5e5
lq-c <2404831604@qq.com>
2024-03-11 00:13:55 +0800

    feat: add demo

---
+++ playground/pages/component/cell.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+const items = ref([
+  {
+    name: 'Vue2',
+    icon: 'star',
+    enabled: false,
+  },
+  {
+    name: 'Vue3',
+    icon: 'heart',
+    enabled: false,
+  },
+  {
+    name: 'React',
+    icon: 'close-circle',
+    enabled: false,
+  },
+])
+
+function handleClick(item) {
+  item.enabled = !item.enabled
+}
+</script>
+
+<template>
+  <var-paper :elevation="2">
+    <var-cell
+      v-for="(item, index) in items" :key="item.name" ripple :icon="item.icon"
+      :border="index !== items.length - 1" :border-offset="0" @click="handleClick(item)"
+    >
+      {{ item.name }}
+
+      <template #extra>
+        <var-switch v-model="item.enabled" @click.stop />
+      </template>
+    </var-cell>
+  </var-paper>
+</template>
