ea7753d25f82590c11fca6ec4d0410006a4aa757
阿菜 Cai <1064425721@qq.com>
2024-05-17 10:19:38 +0800

    test: 增加关于`@varlet/unplugin-icon-builder`的测试 (#18)

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,17 @@
+import path from 'node:path'
 import { defineNuxtConfig } from 'nuxt/config'
 import varlet from '..'
 
+const svgIconsDir = path.resolve(__dirname, './assets/svg-icons')
+
 export default defineNuxtConfig({
-  modules: [varlet],
-  varlet: {},
+  modules: [[
+    varlet,
+    {
+      icon: {
+        dir: svgIconsDir,
+        generatedFilename: './playground/assets/virtual.icons.css',
+      },
+    },
+  ]],
 })
